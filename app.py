bc5555d44b147b1ecd7ffa6abe3031f3385c6552
SpriteUm <Nawaponpop2521@gmail.com>
2025-09-20 03:14:11 +0700

    add login register

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route("/")
 def test():
-    return render_template("index.html")
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
