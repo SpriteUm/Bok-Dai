ea57c763bfe4e25243fca1b47533847f24b5a4a0
patcharapolklinonwork <patcharapolklinon@gmail.com>
2025-09-20 23:31:47 +0700

    เพิ่มตัวกรอง และ ทำให้ฟีเจอร์ใช้งานได้บางส่วย

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def test():
-    return render_template("index.html")
+    return render_template("admin.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
