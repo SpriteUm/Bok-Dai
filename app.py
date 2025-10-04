99e7ce692a97bec7f094ba89312906f76018d80f
SpriteUm <Nawaponpop2521@gmail.com>
2025-10-04 22:04:22 +0700

    edit app.py bug

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager
 from models import db
 from models.user import User
 from routes.report import report_bp
+from routes.auth import auth_bp
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
@@ -10,15 +11,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"   # ย้ำ: endpoint ของ login อยู่ใน blueprint auth
-    login_manager.init_app(app)
-
-    # import models/user here if you have real User model
-    try:
-        from models.user import User as UserModel
-    except Exception:
-        UserModel = None
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"   # ย้ำ: endpoint ของ login อยู่ใน blueprint auth
+login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -30,6 +25,8 @@ def indexuser():
 
 # Register blueprint
 app.register_blueprint(report_bp)
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
 
 if __name__ == "__main__":
     with app.app_context():
